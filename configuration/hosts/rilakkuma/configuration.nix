fa4fa4ec1bd3d6ba19ae6c07942e1cc272de9e05
Antonin Dupont <duponin@locahlo.st>
2020-12-05 14:44:19 +0100

    Fix module importation for rilakkuma

--- configuration/hosts/rilakkuma/configuration.nix
+++ configuration/hosts/rilakkuma/configuration.nix
@@ -6,8 +6,8 @@
 
 {
   imports = [ # Include the results of the hardware scan.
+    ../../modules
     ./hardware-configuration.nix
-    ../../modules/yubikey.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
