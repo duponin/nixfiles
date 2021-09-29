964fed1f188e45cf52f8275a178f00640eeb04e7
Antonin Dupont <duponin@locahlo.st>
2021-09-29 10:12:50 +0200

    Fix postgresql break

--- configuration/hosts/reimu/postgresql.nix
+++ configuration/hosts/reimu/postgresql.nix
@@ -2,8 +2,4 @@
 
 {
   services.postgresql.enable = true;
-  services.postgresql.authentication.authentication = ''
-    host all all 127.0.0.1/32 trust
-    host all all ::1/128      trust
-  '';
 }
