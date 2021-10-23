55f4742b96a2ac024bf411507f667e57bed20d8d
Antonin Dupont <duponin@locahlo.st>
2021-10-23 21:39:24 +0200

    Format + add missing patch
    Why… I'm reeeeeeeeeeeeeeeeeeeeee

--- configuration/hosts/sarah/hardware-configuration.nix
+++ configuration/hosts/sarah/hardware-configuration.nix
@@ -1,24 +1,15 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" "wl" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "broadcom-sta"
-  ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/dc2b3f7e-8a05-4555-90d1-aa0e4c1c1282";
-      fsType = "ext4";
-    };
-
-  swapDevices = [ ];
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [ "broadcom-sta" ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/dc2b3f7e-8a05-4555-90d1-aa0e4c1c1282";
