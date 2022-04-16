2825e31140c8f5586ee1209e06425057fbea3a91
Antonin Dupont <contact+git@antonindupont.fr>
2022-04-16 13:15:56 +0200

    import sourcehut

--- configuration/hosts/enceladus/configuration.nix
+++ configuration/hosts/enceladus/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./sourcehut.nix
       ../../common/flakes.nix
       ../../common/server.nix
     ];
