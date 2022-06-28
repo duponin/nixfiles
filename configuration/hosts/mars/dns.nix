36ad7f0057df7b24f5e566a364a69ce4262b799f
Antonin Dupont <contact+git@antonindupont.fr>
2022-06-28 21:53:19 +0200

    missig @ in NS record

--- configuration/hosts/mars/dns.nix
+++ configuration/hosts/mars/dns.nix
@@ -22,7 +22,7 @@
                ; Negative Cache TTL
                604800 )
 
-          IN NS ns1.localhost.net.
+          @ IN NS ns1.localhost.net.
 
           mars IN A 185.233.102.155
           mars IN AAAA 2a0c:e300:12::155
