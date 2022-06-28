95ebe334b42523f8d24b11c3babc0a07c47987ab
Antonin Dupont <contact+git@antonindupont.fr>
2022-06-28 18:37:44 +0200

    DNS on mars

--- configuration/hosts/mars/configuration.nix
+++ configuration/hosts/mars/configuration.nix
@@ -5,6 +5,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./dns.nix
       ../../common/flakes.nix
       ../../common/server.nix
     ];
