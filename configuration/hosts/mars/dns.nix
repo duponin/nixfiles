c8ba17dbee6507f2bd861d25f569ac825af6b86f
Antonin Dupont <contact+git@antonindupont.fr>
2022-06-28 20:58:14 +0200

    change bind for nsd

--- configuration/hosts/mars/dns.nix
+++ configuration/hosts/mars/dns.nix
@@ -2,15 +2,30 @@
 
 {
   networking.firewall.allowedUDPPorts = [ 53 ];
-  services.bind = {
+  services.nsd = {
     enable = true;
     zones = {
-      "locahlost.net" = {
-        extraConfig = "";
-        file = "/var/dns/locahlost.net";
-        master = true;
-        masters = [ ];
-        slaves = [ ];
+      "locahlost.net." = {
+        data = ''
+          $ORIGIN locahlost.net.
+          $TTL 3600
+          @ IN SOA ns1.locahlost.net. admin.locahlost.net. (
+               ; Serial
+               1
+               ; Refresh
+               604800
+               ; Retry
+               86400
+               ; Expire
+               2419200
+               ; Negative Cache TTL
+               604800 )
+
+          IN NS ns1.localhost.net.
+
+          mars IN A 185.233.102.155
+          mars IN AAAA 2a0c:e300:12::155
+        '';
       };
     };
   };
