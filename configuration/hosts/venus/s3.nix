c9d258e58f38fe2b5611f59d4bcfeff6ba827a3f
Antonin Dupont <contact+git@antonindupont.fr>
2022-06-29 21:13:00 +0200

    minio needs a redirect URL for the console

--- configuration/hosts/venus/s3.nix
+++ configuration/hosts/venus/s3.nix
@@ -5,6 +5,8 @@
     enable = true;
     region = "fr-bordeaux-1";
   };
+  systemd.services.minio.environment.MINIO_BROWSER_REDIRECT_URL =
+    "https://console.s3.locahlo.st/";
   networking.firewall.interfaces.ens19.allowedTCPPorts = [
     9000 # Minio service port
     9001 # Minio console port
