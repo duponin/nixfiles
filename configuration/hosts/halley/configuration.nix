8742c361a5451bc0f3e1aeb5dba0c7cd079cdbe2
duponin <duponin@locahlo.st>
2022-10-21 20:23:00 +0200

    Common workstation configuration is put in common

--- configuration/hosts/halley/configuration.nix
+++ configuration/hosts/halley/configuration.nix
@@ -6,6 +6,7 @@
       ./hardware-configuration.nix
       ./users.nix
       ./tuning.nix
+      ./workstation.nix
       ../../common/flakes.nix
     ];
 
@@ -20,7 +21,6 @@
   };
 
   networking.hostName = "halley"; # Define your hostname.
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Paris";
@@ -39,45 +39,6 @@
     LC_TELEPHONE = "fr_FR.utf8";
     LC_TIME = "fr_FR.utf8";
   };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "fr";
-    xkbVariant = "bepo";
-  };
-
-  # Configure console keymap
-  console.keyMap = "fr-bepo-latin9";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   environment.systemPackages = with pkgs; [
     vim
