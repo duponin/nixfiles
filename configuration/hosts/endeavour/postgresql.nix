db626423076cd98a9540194c995805743b5a6864
duponin <duponin@locahlo.st>
2023-01-01 21:13:29 +0100

    pluto replicator

--- configuration/hosts/endeavour/postgresql.nix
+++ configuration/hosts/endeavour/postgresql.nix
@@ -9,7 +9,23 @@
     extraPlugins = with pkgs.postgresql_14.pkgs; [ pg_repack ];
     settings = {
       max_wal_size = "4GB";
+      wal_level = "logical";
+      # Set to twice as many replicas as you ever expect to have.
+      # https://postgresqlco.nf/doc/en/param/max_replication_slots/
+      # max_replication_slots = 40;
     };
+    authentication = ''
+      host all pluto_replicator 192.168.0.47/24 trust
+    '';
+    ensureUsers = [
+      {
+        name = "pluto_replicator";
+        ensurePermissions = {
+          "SCHEMA public" = "USAGE";
+          "ALL TABLES IN SCHEMA public" = "SELECT";
+        };
+      }
+    ];
     ensureDatabases = [
       "udongein_pleroma"
     ];
