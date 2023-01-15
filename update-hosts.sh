8f27efa1de2022892f5d83b936f878b56993faf9
duponin <duponin@locahlo.st>
2023-01-15 12:18:18 +0100

    try updating the update script
    I rather should use a real deployment tool

--- update-hosts.sh
+++ update-hosts.sh
@@ -2,42 +2,15 @@
 # shellcheck disable=SC2029
 # shellcheck disable=SC2039
 
-GREEN="\e[0;32m"
-RESET="\e[m"
-ORANGE="\e[0;33m"
-BLUE="\e[0;94m"
+verb=$1
 
-echo_cmd () {
-    echo -e "${BLUE}→ ${1} ${RESET}"
-}
-
-echo_and_ssh () {
-    (
-        echo_cmd "${2}"
-        ssh "${1}" "${2}"
-    )
-}
-
-(
-    cd configuration/hosts || exit
-    #TODO use gnu/parrallel or something else to accelerate runtime once I'll have more hosts
-    for host in * ; do 
-        if [ "$host" = "$(hostname)" ]; then
-            sleep 0.1
-        else
-            (
-                echo -e ""
-                echo -e "${GREEN}#=================================================================="
-                echo -e "# $host ${RESET}"
-                echo -e "${ORANGE}#-------- git pull -------${RESET}"
-
-                echo_and_ssh "$host" "sudo git -C /etc/nixfiles/ config pull.ff only"
-                echo_and_ssh "$host" "sudo git -C /etc/nixfiles/ pull" 
-
-                echo -e "${ORANGE}#-------- nixos-rebuild switch -------${RESET}"
-
-                echo_and_ssh "$host" "sudo /etc/nixfiles/nixos-rebuild.sh switch"
-            )
-        fi
-    done
-)
+for host in $(ls configuration/hosts) ; do
+    echo "######################################################"
+    echo $host
+    ping -c 1 -W 2 "${host}.locahlost.net" > /dev/null && if [ "$host" = "$(hostname)" ]; then
+        echo "# Skipping ${host}, reason: localhost"
+    else
+        nixos-rebuild $verb --use-remote-sudo --flake ".#${host}" --target-host "${host}.locahlost.net"
+    fi
+    echo "#-----------------------------------------------------"
+done
