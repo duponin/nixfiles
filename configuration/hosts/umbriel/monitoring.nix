f2c4ad9375ec106b31802eff6cbe463834c8a724
duponin <duponin@locahlo.st>
2023-01-19 21:25:13 +0100

    PostgreSQL exporter on umbriel

--- configuration/hosts/umbriel/monitoring.nix
+++ configuration/hosts/umbriel/monitoring.nix
@@ -7,12 +7,15 @@
     prometheus.exporters = {
       nginx.enable = true;
       node.enable = true;
+      postgres.enable = true;
+      postgres.user = "postgres";
     };
 
     nginx.enable = true;
     nginx.virtualHosts."${config.networking.fqdn}".locations = {
       "/prometheus/nginx/".proxyPass = "http://localhost:${toString config.services.prometheus.exporters.nginx.port}/";
       "/prometheus/node-exporter/".proxyPass = "http://localhost:${toString config.services.prometheus.exporters.node.port}/";
+      "/prometheus/postgres/".proxyPass = "http://localhost:${toString config.services.prometheus.exporters.postgres.port}/";
     };
   };
 }
