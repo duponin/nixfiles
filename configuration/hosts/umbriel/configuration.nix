0bd78b7904abf9165ed627e9099a48faf43b6fed
duponin <duponin@locahlo.st>
2023-01-28 15:35:01 +0100

    basic gitea service @ umbriel

--- configuration/hosts/umbriel/configuration.nix
+++ configuration/hosts/umbriel/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./gitea.nix
     ./monitoring.nix
     ./pleroma.nix
     ./postgresql.nix
---
+++ configuration/hosts/umbriel/gitea.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  services.mysql.enable = true;
+  services.mysql = {
+    ensureDatabases = [
+      "udongein_gitea"
+    ];
+    ensureUsers = [
+      {
+        name = "udongein_gitea";
+        ensurePermissions = {
+          "udongein_gitea.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+  };
+
+  services.gitea = {
+    enable = true;
+    domain = "dev.udongein.xyz";
+    rootUrl = "https://dev.udongein.xyz/";
+    appName = "Udongein Hub";
+    database = {
+      user = "udongein_gitea";
+      passwordFile = "/srv/gitea/db-password.txt";
+      host = "localhost";
+      name = "udongein_gitea";
+      createDatabase = false;
+    };
+  };
+}
